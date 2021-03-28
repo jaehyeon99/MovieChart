964a2639628da05ab38f9bae495df36c30372ea3
jaehyeon99 <janhyun99@naver.com>
2021-03-28 23:41:28 +0900

    complete moviechart UI

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Navbar } from 'react-bootstrap';
+
+
+function App() {
+  let [moviedata, adddata] = useState(null);
+  let [date, setdate] = useState("20200302");
+  let [inputdate, changedate] = useState(" ");
+
+
+  useEffect(() => {
+    axios.get("http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=" + (date))
+      .then(result => adddata(result.data.boxOfficeResult.dailyBoxOfficeList))
+      .catch(e => console.log(e));
+  }, [date]); // date가 변함에 따라 useEffect 한번더 렌더링
+
+
+  if (!moviedata) { //moviedata가 null이라면 LOADING을 반환해라 CLEAR
+    return <div>Loading</div>
+  }
+
+
+
+
+  return (
+    <div className="App">
+
+      {
+        console.log(moviedata)
+      }
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Brand className="text-light" href="#home">Movie Charts</Navbar.Brand>
+      </Navbar>
+      <>
+        <div className="table">
+          <input onChange={(e) => {
+            changedate(e.target.value)
+          }} placeholder="원하는 날짜 입력 ex> 20200329" />
+          <button onClick={() => {
+            setdate(inputdate);
+          }}>검색하기</button>
+          {
+            moviedata.map((movie, i) => {
+              return (
+                <>
+                  <p>{movie.rank} 위</p>
+                  <hr />
+                  <h6>{movie.movieNm} ({movie.openDt} 개봉)</h6>
+                  <hr />
+
+                </>
+              )
+            })
+          }
+        </div>
+      </>
+
+    </div >
+  );
+}
+
+export default App;
---
+++ src/Movier.js
@@ -0,0 +1,35 @@
+// import React from 'react'
+// import { Table } from 'react-bootstrap';
+
+// function Movier(props) {
+//     return (
+
+//         <Table variant="dark">
+//             <thead>
+//                 <tr>
+//                     <th>순위</th>
+//                     <th>영화 제목</th>
+//                     <th>개봉 일자 </th>
+
+//                 </tr>
+//             </thead>
+
+//             <tbody>
+//                 <tr>
+//                     <td>
+//                         {
+//                             props.a.map(() => {
+//                                 <p>{props.moviedata.rank}</p>
+
+//                             })
+//                         }
+//                     </td>
+
+//                 </tr>
+//             </tbody>
+//         </Table>
+
+//     )
+// }
+
+// export default Movier;
